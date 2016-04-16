53f5819dccfbbed7c3a731cc48f46e812d0b1db4
BadPinkChicken <salnikoff.a@gmail.com>
2016-04-16 14:49:24 +0200

    added random event

--- PowerEvent.cpp
+++ PowerEvent.cpp
@@ -2,6 +2,13 @@
 
 PowerEvent::PowerEvent()
 {
+    this->font.loadFromFile("ressources/talldark.ttf");
+    this->display = false;
+    this->text.setFont(this->font);
+    this->text.setStyle(sf::Text::Bold);
+    this->textKeys.setFont(this->font);
+    this->textKeys.setStyle(sf::Text::Bold);
+    this->textKeys.setColor(sf::Color::Yellow);
     sf::Shape   *block1 = new sf::RectangleShape(sf::Vector2f(50, 50));
     sf::Texture *tmp = new sf::Texture();
     if (!tmp->loadFromFile("ressources/hulkBlock.png"))
@@ -22,6 +29,12 @@ PowerEvent::PowerEvent()
     if (!tmp->loadFromFile("ressources/rabbitBlock.png"))
         std::cout << "Error texture hulkBlock" << std::endl;
     block4->setTexture(tmp);
+    backBlock = new sf::RectangleShape(sf::Vector2f(300, 70));
+    tmp = new sf::Texture();
+    if (!tmp->loadFromFile("ressources/backblock.png"))
+        std::cout << "error texture back block" << std::endl;
+    backBlock->setTexture(tmp);
+    backBlock->setPosition(sf::Vector2f(480, 190));
     for (int i = 0; i < 4; i++)
         this->keys.push_back(i+1);
     this->PowerList.push_back(block1);
@@ -41,28 +54,41 @@ static bool     isInList(std::list<int> &list, int nb)
 
 void    PowerEvent::newEvent()
 {
+    this->display = true;
     this->killEvent();
     std::list<int>  tmp;
     std::list<int>  tmp2;
+    int i = 0;
     int nb = rand() % 4 + 1;
+    std::string tmpString;
     this->time = 10;
-   /* for (int i = 0; i < 3; i++){
-        while (isInList(this->CurrentKeys, nb))
-            nb = rand() % 4 + 1;
-        this->CurrentKeys.push_back(nb);
-    }*/
     nb = rand() % 4;
     for (int i = 0; i < 3; i++){
         while (isInList(tmp, nb))
             nb = rand() % 4;
         tmp.push_back(nb);
     }
+    nb = rand() % 4 + 1;
+    for (int i = 0; i < 3; i++){
+        while (isInList(tmp2, nb))
+            nb = rand() % 4 + 1;
+        tmp2.push_back(nb);
+    }
+
+    i = 0;
+     for (std::list<int>::iterator it = tmp2.begin(); it != tmp2.end(); it++) {
+        this->CorrespKeys[i] = *it;
+
+        if (i =! 0)
+            tmpString += "           ";
+        tmpString += patch::to_string(*it);
+        i++;
+    }
     for (std::list<int>::iterator it = tmp.begin(); it != tmp.end(); it++) {
         this->CurrentPower[*it] = this->PowerList[*it];
-        //this->CurrentCorresp.push_back(*it);
     }
-    for (std::map<int, sf::Shape *>::iterator it = this->CurrentPower.begin(); it != this->CurrentPower.end(); it++)
-        std::cout << it->first << std::endl;
+
+    this->textKeys.setString(tmpString);
 }
 
 int     PowerEvent::getTime() const
@@ -93,8 +119,20 @@ void    PowerEvent::killEvent()
 
 void    PowerEvent::update(int time, sf::RenderWindow & win)
 {
+    static  int last_time;
     int i = 0;
     (void) time;
+
+    if (!this->display)
+        return;
+    this->text.setCharacterSize(64);
+    this->text.setPosition(sf::Vector2f(610, 120));
+    this->textKeys.setPosition(sf::Vector2f(440, 250));
+    this->text.setColor(sf::Color::Yellow);
+    this->text.setString(patch::to_string(this->time));
+    win.draw(this->text);
+    win.draw(*backBlock);
+    win.draw(this->textKeys);
     for (std::map<int, sf::Shape *>::iterator it = this->CurrentPower.begin(); it != this->CurrentPower.end(); it++)
     {
         it->second->setPosition(sf::Vector2f(i * 100 + 500, 200));
