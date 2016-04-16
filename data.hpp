afd28f36a58026e808cfff3ef65ea1bb96d24bde
BadPinkChicken <salnikoff.a@gmail.com>
2016-04-16 11:39:49 +0200

    merge + ajout fond

--- data.hpp
+++ data.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
-#define DEBUG true
-#define HEIGHT 600
-#define WIDTH 1200
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#include <iostream>
+
+#define DEBUG   true
+#define HEIGHT  600
+#define WIDTH   1200
+#define SPEED   25
--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
+
 
 #include "data.hpp"
+#include "back.hpp"
 
 int main()
 {
     sf::RenderWindow    window(sf::VideoMode(WIDTH,HEIGHT), "SFML");
-    sf::CircleShape     shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    Background          back1("ressources/background.png", "ressources/ground.png", sf::Vector2f(WIDTH, HEIGHT), sf::Vector2f(WIDTH, 64), 0);
+    Background          back2("ressources/background.png", "ressources/ground.png", sf::Vector2f(WIDTH, HEIGHT), sf::Vector2f(WIDTH, 64), WIDTH);
+    //window.setFramerateLimit(60);
     while (window.isOpen())
         {
             sf::Event   event;
 
-            while (window.pollEvent(event))
+           while (window.pollEvent(event))
             {
                 if (event.type == sf::Event::Closed)
                     window.close();
             }
             window.clear();
-            window.draw(shape);
+            back1.update(window);
+            back2.update(window);
             window.display();
         }
     return 0;
