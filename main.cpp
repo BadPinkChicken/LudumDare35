d3f5c4f2cec0128510d7c8aa85b0dd3178ebc657
Salnikoff Alexis <alexis.salnikoff@epitech.eu>
2016-04-16 17:49:41 +0200

    Push de bourrin

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "Hulk.hpp"
 #include "Captain.hpp"
 #include "JumpObstacle.hpp"
+#include "Duck.hpp"
 #include <ctime>
 #include <SFML/Audio.hpp>
 
@@ -34,68 +35,83 @@ int main()
     Background          back1("ressources/background.png", "ressources/ground.png", sf::Vector2f(WIDTH, HEIGHT), sf::Vector2f(WIDTH, 64), 0);
     Background          back2("ressources/background.png", "ressources/ground.png", sf::Vector2f(WIDTH, HEIGHT), sf::Vector2f(WIDTH, 64), WIDTH);
     PowerEvent          events;
-    ACharacter *humain = new Rabbit();
-    sf::Time timee;
-    sf::Clock   total;
-    sf::Clock frameClock;
-    sf::SoundBuffer buffer;
-    sf::Sound sound;
-    CHARTYPE   transformation;
-    AObstacle *obstacle = new JumpObstacle(300, 100);
-    obstacle->init();
-    int last_time = 500000;
+  AObstacle		*obstacle = new JumpObstacle(30, 10);
+  obstacle->init();
+  ACharacter	*humain = new Humain();
+  ACharacter	*rabbit = new Rabbit();
+  ACharacter	*hulk = new Hulk();
+  ACharacter	*duck = new Duck();
+  ACharacter	*current = humain;
 
-    window.setFramerateLimit(60);
+  (void) rabbit;
+  (void) hulk;
+  sf::Time timee;
+  sf::Clock   total;
+  sf::Clock frameClock;
+  sf::SoundBuffer buffer;
+  sf::Sound sound;
+  CHARTYPE   transformation;
 
-    srand(time(NULL));
-    if (!buffer.loadFromFile("ressources/Avengers.wav"))
-      return -1;
-    sound.setBuffer(buffer);
-    sound.setLoop(true);
-    sound.play();
-    while (window.isOpen())
+  int last_time = 500000;
+
+  window.setFramerateLimit(60);
+
+  srand(time(NULL));
+  if (!buffer.loadFromFile("ressources/Avengers.wav"))
+    return -1;
+  sound.setBuffer(buffer);
+  sound.setLoop(true);
+  sound.play();
+  while (window.isOpen())
+    {
+      timee = frameClock.restart();
+      sf::Event   event;
+
+      while (window.pollEvent(event))
 	{
-	    timee = frameClock.restart();
-	    sf::Event   event;
-
-	   while (window.pollEvent(event))
+	  if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape)
+	    window.close();
+	  if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::E)
+	    events.newEvent();
+	  if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R)
+	    current = humain;
+	  if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::T)
+	    current = rabbit;
+	  if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Y)
+	    current = hulk;
+	  if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::U)
+	    current = duck;
+	  if (events.getDisplay())
 	    {
-		if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape)
-		    window.close();
-		if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::E)
-		    events.newEvent();
-		if (events.getDisplay())
-		  {
-		    if (event.type == sf::Event::KeyPressed)
-		      {
-			if (event.key.code == sf::Keyboard::Num1)
-			  transformation = events.getBlockType(1);
-			else if (event.key.code == sf::Keyboard::Num2)
-			  transformation = events.getBlockType(2);
-			else if (event.key.code == sf::Keyboard::Num3)
-			  transformation = events.getBlockType(3);
-			else if (event.key.code == sf::Keyboard::Num4)
-			  transformation = events.getBlockType(4);
-		      }
-		  }
+	      if (event.type == sf::Event::KeyPressed)
+		{
+		  if (event.key.code == sf::Keyboard::Num1)
+		    transformation = events.getBlockType(1);
+		  else if (event.key.code == sf::Keyboard::Num2)
+		    transformation = events.getBlockType(2);
+		  else if (event.key.code == sf::Keyboard::Num3)
+		    transformation = events.getBlockType(3);
+		  else if (event.key.code == sf::Keyboard::Num4)
+		    transformation = events.getBlockType(4);
+		}
 	    }
-
-	    handleEvents(humain, timee, back1, back2);
+	}
+	    handleEvents(current, timee, back1, back2);
 	    if (total.getElapsedTime().asMicroseconds() - last_time > 6000000 && !events.getDisplay())
 	      {
 		events.newEvent();
 		last_time = total.getElapsedTime().asMicroseconds();
 	      }
 
-	    window.clear();
-	    back1.update(window);
-	    back2.update(window);
-        obstacle->update(window);
-        if (obstacle->checkPlayerCollision(*humain) == true)
-            exit(0);
-	    events.update(total.getElapsedTime().asMicroseconds(), window);
-	    humain->move(ACharacter::RIGHT, sf::Vector2f(0, 0), timee, back1.getGround(), back2.getGround());
-	    window.draw(humain->getAnimatedSprite());
+      window.clear();
+      back1.update(window);
+      back2.update(window);
+      obstacle->update(window);
+      if (obstacle->checkPlayerCollision(*current) == true)
+	exit(0);
+      events.update(total.getElapsedTime().asMicroseconds(), window);
+	    current->move(ACharacter::RIGHT, sf::Vector2f(0, 0), timee, back1.getGround(), back2.getGround());
+	    window.draw(current->getAnimatedSprite());
 	    window.display();
 	}
     return 0;
