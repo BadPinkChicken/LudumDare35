138e676093a4dcd1e0e0bbee27979be597f6770b
bocque_c <you@example.com>
2016-04-17 13:59:24 +0200

    Fire

--- Humain.cpp
+++ Humain.cpp
@@ -10,17 +10,17 @@ Humain::Humain() : ACharacter()
   this->_animate = new AnimatedSprite(sf::seconds(0.1), true, false);
   (*this->_animations)[RIGHT].setSpriteSheet(*this->_texture);
   //this->setFrames(RIGHT, 70, 90, 0, 6, 0);
-  this->setFrames(RIGHT, 125, 106, 0, 6, 0);
+  this->setFrames(RIGHT, 125, 125, 0, 6, 0);
   (*this->_animations)[UP].setSpriteSheet(*this->_texture);
 
-  this->setFrames(UP, 125, 106, 0, 6, 0);
+  this->setFrames(UP, 125, 125, 0, 6, 0);
 
   //this->setFrames(UP, 70, 90, 0, 6, 0);
   this->_x = 0;
   this->_y = 0;
-  this->_speed = 700;
+  this->_speed = 900;
   this->_strengh = false;
-  this->_jumpHeight = 300;
+  this->_jumpHeight = 250;
   this->_current = &((*this->_animations)[RIGHT]);
   this->_animate->setPosition(sf::Vector2f(screenDimensions));
   this->_animate->play(*this->_current);
